d270a0074789f3540101688cb4bbaedb835c5e6e
Bryn Williams <bryn2009@live.co.uk>
2023-08-16 18:23:28 +0100

    tests for controller and service

--- src/test/java/com/example/playerTracker/PlayerControllerTest.java
+++ src/test/java/com/example/playerTracker/PlayerControllerTest.java
@@ -1,2 +1,36 @@
-package com.example.playerTracker;public class PlayerControllerTest {
+package com.example.playerTracker;
+
+import com.example.playerTracker.controller.PlayerController;
+import com.example.playerTracker.model.Player;
+import com.example.playerTracker.service.PlayerService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.Arrays;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(PlayerController.class)
+class PlayerControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PlayerService playerService;
+
+    @Test
+    void testGetAllPlayers() throws Exception {
+        when(playerService.getAllPlayers()).thenReturn(Arrays.asList(new Player("Bryn", 27, "ST", 80, 90, 70, 85)));
+
+        mockMvc.perform(get("/api/players"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(1));
+    }
+
 }
+
--- src/test/java/com/example/playerTracker/PlayerServiceTests.java
+++ src/test/java/com/example/playerTracker/PlayerServiceTests.java
@@ -1,2 +1,22 @@
-package com.example.playerTracker;public class PlayerServiceTests {
+package com.example.playerTracker;
+
+import com.example.playerTracker.model.Player;
+import com.example.playerTracker.service.PlayerServiceImpl;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+class PlayerServiceTests {
+
+    @Test
+    void testGetAllPlayers() {
+        PlayerServiceImpl playerService = new PlayerServiceImpl();
+        playerService.addPlayer(new Player("Bryn", 27, "CB", 80, 90, 70, 85));
+        playerService.addPlayer(new Player("John", 25, "ST", 75, 85, 65, 80));
+
+        List<Player> players = playerService.getAllPlayers();
+
+        assertEquals(2, players.size());
+    }
+
 }
